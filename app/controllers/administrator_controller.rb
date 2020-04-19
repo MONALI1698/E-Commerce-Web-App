0fe71095cd19964dff0387d5f0eec491065f1887
Maulika <monali.trivedi98@gmail.com>
2020-04-19 01:24:58 -0500

    Completed iss4

---
+++ app/controllers/administrator_controller.rb
@@ -0,0 +1,58 @@
+class AdministratorController < ApplicationController
+    before_action :authenticate_user!
+    before_action :is_admin?, only: [:index]
+    
+    before_action :require_permission, only: [ :edit, :update, :destroy]
+
+        
+    def index
+        itemlist = Item.includes(:creator).all
+        respond_to do |format|
+            format.html { render:index, locals: { itemlist:itemlist} }
+        end
+    end
+
+    def show
+        item = nil
+        if(params[:id] != "search_results")
+            item = Item.find(params[:id])
+        end
+
+        respond_to do |format|
+            format.html do 
+                if(params[:id] == "search_results")
+                    flash[:error] = "You must enter a value to search."
+                    redirect_back(fallback_location: root_path)
+                else
+                 render:show, locals: { item:item} 
+                end
+            end
+        end
+    end
+
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+        respond_to do |format|
+            format.html do
+                flash[:success] = 'Item removed successfully'
+                redirect_to admin_url
+            end
+        end
+    end
+
+    def require_permission
+    
+        if !current_user.is_admin
+          redirect_to items_path, flash: { error: "You do not have permission to do that." }
+        end
+    end
+
+    
+    def is_admin?
+        if(!current_user.is_admin)
+            redirect_to :home, flash: { error: "You must be an administrator to view all items." }
+        end
+    end
+
+end
